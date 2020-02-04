48818c529315250d78f246a940e9b9a2c851fcbb
Louis Alonzo <louis.a@live.com>
2020-02-03 21:20:42 -0800

    created a seeds file for database

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger',true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger',true);
