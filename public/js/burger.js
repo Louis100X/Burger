708416f53c2bc24062e03e2ac5f783177682c7c6
Louis Alonzo <louis.a@live.com>
2020-02-04 17:06:19 -0800

    folder

---
+++ public/js/burger.js
@@ -0,0 +1,62 @@
+$(function() {
+    $(".devour-burger").on("click", function(event) {
+      var id = $(this).data("id");
+      var devouredBurger = true;
+      console.log(devouredBurger);
+  
+      var newDevouredState = {
+        devoured: devouredBurger
+      };
+      console.log(newDevouredState);
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", devouredBurger);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#newBurgerCreated").val().trim(),
+        devoured: false
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Created A New Burger:" + newBurger);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted cat", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
